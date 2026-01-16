717df2b80ae9b06f7be4dde39fd39898c1776451
Abhisheksinghh7 <ping2abhishekk@gmail.com>
2026-01-16 14:58:24 +0530

    Add files via upload

--- adl-utils.js
+++ adl-utils.js
@@ -325,6 +325,7 @@ window.adl.trackPurchase = function(order) {
     xdm: {
       commerce: {
         orderID: order.orderID || "",
+        email: order.email || "",
         productListItems: (order.products || order || []).map(p => {
           const complete = window.adl.getCompleteProductData(p);
           const discountAmount = complete.discount ? (complete.price * complete.discount / 100) : 0;
